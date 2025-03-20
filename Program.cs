9119203c212300f80b11e155617e0f6b9f40c023
markdicks <markdicks03@gmail.com>
2025-03-20 06:37:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using InstagramDataViewer;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");  // Ensure the root component is linked to the correct element in the HTML
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
